b596abde7bfadd679582ac543021967bc231f6a3
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-03-26 17:17:31 +0300

    Смена просмотра

--- page/gvs/script/statistics.js
+++ page/gvs/script/statistics.js
@@ -3,8 +3,8 @@ let statistics = document.getElementById('statistics');
 // владею кол-во монет
 let have = 0;
 
-for(i=0; i<GVSiHaveTest.length; i++){
-    if(GVSiHaveTest[i].HAVE){
+for (i = 0; i < GVSiHaveTest.length; i++) {
+    if (GVSiHaveTest[i].HAVE) {
         have++;
         console.log(GVSiHaveTest[i].NAME)
     }
@@ -16,10 +16,41 @@ statistics.innerHTML = `${have} / ${GVSiHaveTest.length}`;
 // Постройка блоков
 let cluster = document.getElementById('cluster');
 
-for(u=0; u<55; u++){
-	cluster.innerHTML += `
-		<a href="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg">
+for (u = 0; u < GVSiHaveTest.length; u++) {
+    cluster.innerHTML += `
+		<a class="card" href="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg">
 			<img src="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg" >
 		</a>
 `
 }
+
+
+
+// проссмотр (список)
+function list() {
+    cluster.innerHTML = "";
+    cluster.className = "lists";
+    for (u = 0; u < GVSiHaveTest.length; u++) {
+        cluster.innerHTML += `
+		<div class="list" href="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg">
+			<img src="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg">
+            <div>${GVSiHaveTest[u].NAME}</div>
+            <div>${GVSiHaveTest[u].NUMB}</div>
+            <div>${GVSiHaveTest[u].DATE}</div>
+		</div>
+`
+    }
+};
+
+// проссмотр (список)
+function card() {
+    cluster.innerHTML = "";
+    cluster.className = "cards";
+    for (u = 0; u < GVSiHaveTest.length; u++) {
+        cluster.innerHTML += `
+		<a class="card" href="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg">
+			<img src="img/gvs/${GVSiHaveTest[u].NUMB}r.jpg" >
+		</a>
+`
+    }
+};
